53bdefd9e3e46e1114e90ea3e723adbc21983027
Wojtazzzz <marcin.witas72@gmail.com>
2025-02-08 15:48:23 +0100

    chore: change sqlite3 to postgres

--- config/dev.exs
+++ config/dev.exs
@@ -2,10 +2,14 @@ import Config
 
 # Configure your database
 config :live_trades, LiveTrades.Repo,
-  database: Path.expand("../live_trades_dev.db", __DIR__),
-  pool_size: 5,
+  username: "postgres",
+  password: "postgres",
+  hostname: "localhost",
+  database: "live_trades",
   stacktrace: true,
-  show_sensitive_data_on_connection_error: true
+  show_sensitive_data_on_connection_error: true,
+  pool_size: 10,
+  port: 5432
 
 # For development, we disable any cache and enable
 # debugging and code reloading.
--- lib/live_trades/repo.ex
+++ lib/live_trades/repo.ex
@@ -1,5 +1,5 @@
 defmodule LiveTrades.Repo do
   use Ecto.Repo,
     otp_app: :live_trades,
-    adapter: Ecto.Adapters.SQLite3
+    adapter: Ecto.Adapters.Postgres
 end
