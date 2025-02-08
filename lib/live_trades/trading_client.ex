9c4a01a221a7b6dafbfce7d566f84e866ca411f2
Wojtazzzz <marcin.witas72@gmail.com>
2025-02-08 16:34:33 +0100

    feat: add http client for fetch trading data

---
+++ lib/live_trades/trading_client.ex
@@ -0,0 +1,22 @@
+defmodule LiveTrades.TradingClient do
+  def fetch_ticker_data_by_code(code) do
+    with {:ok, %HTTPoison.Response{status_code: 200, body: body}} <-
+           HTTPoison.get(
+             "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=#{String.upcase(code)}&apikey=0UK6V2NVUXRJ1JDX"
+           ),
+         {:ok, data} = Jason.decode(body) do
+      global_quote = Map.get(data, "Global Quote", %{})
+
+      IO.inspect(data)
+
+      %{
+        price: Map.get(global_quote, "05. price", "N/A"),
+        symbol: Map.get(global_quote, "01. symbol", "N/A"),
+        change: Map.get(global_quote, "09. change", "N/A"),
+        open: Map.get(global_quote, "02. open", "N/A")
+      }
+    else
+      _ -> {:error, :failed_to_fetch_data}
+    end
+  end
+end
