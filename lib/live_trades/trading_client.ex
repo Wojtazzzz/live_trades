59c8b1566d85e0a3ed05e6d8ce3ee5c3fe5547d7
Wojtazzzz <marcin.witas72@gmail.com>
2025-02-10 18:47:08 +0100

    refactor: remove commented implementation of fetch data through alpha vantage

--- lib/live_trades/trading_client.ex
+++ lib/live_trades/trading_client.ex
@@ -1,25 +1,4 @@
 defmodule LiveTrades.TradingClient do
-  # def fetch_company_data_by_code(code) do
-  #   with {:ok, %HTTPoison.Response{status_code: 200, body: body}} <-
-  #          HTTPoison.get(
-  #            "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=#{String.upcase(code)}&apikey=#{System.get_env("ALPHA_VANTAGE_API_KEY")}"
-  #          ),
-  #        {:ok, data} = Jason.decode(body) do
-  #     global_quote = Map.get(data, "Global Quote", %{})
-
-  #     IO.inspect(data)
-
-  #     %{
-  #       price: Map.get(global_quote, "05. price", "N/A"),
-  #       symbol: Map.get(global_quote, "01. symbol", "N/A"),
-  #       change: Map.get(global_quote, "09. change", "N/A"),
-  #       open: Map.get(global_quote, "02. open", "N/A")
-  #     }
-  #   else
-  #     _ -> {:error, :failed_to_fetch_data}
-  #   end
-  # end
-
   def fetch_company_data_by_code(code) do
     with {:ok, %HTTPoison.Response{status_code: 200, body: body}} <-
            HTTPoison.get(
